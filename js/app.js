f1ef39af7bb90afbf51adfe459dab42bfb64d6d1
vishal-picode <vishal@picode.in>
2023-09-05 14:57:37 +0530

    add demos

--- js/app.js
+++ js/app.js
@@ -29,10 +29,10 @@
           const now = new Date().getTime(),
                 distance = countDown - now;
   
-          document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+          if(document.getElementById("days"))document.getElementById("days").innerText = Math.floor(distance / (day))
+            if(document.getElementById("hours"))document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour))
+           if(document.getElementById("minutes"))document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute))
+           if(document.getElementById("seconds"))document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second)
   
           //do something later when date is reached
           if (distance < 0) {
